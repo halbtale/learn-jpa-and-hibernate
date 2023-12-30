6a262ba2605d0143891d4e4f663cd3a088167de9
Alberto <42894888+Nenino2@users.noreply.github.com>
2023-12-30 22:04:58 +0100

    first commit

---
+++ src/test/java/com/alberto/learnjpaandhibernate/LearnJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.learnjpaandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
